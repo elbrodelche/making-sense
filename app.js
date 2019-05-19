b9ef4e912c10d05eaaa39fbbaa3f4171cf04075d
Juan Carlos Migliavacca <juancarlos@xeonworks.com>
2019-05-19 11:34:55 -0300

    Implement database and environment setup. First Schema.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+require('dotenv').config({
+  path: `./env-files/${process.env.NODE_ENV || 'development'}.env`,
+});
+
 const express = require('express');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+const postRouter = express.Router();
+
+postRouter.route('/posts').get((req, res) => {
+  const response = { hello: 'Im a post' };
+  res.json(response);
+});
+
+app.use(process.env.API_VERSION, postRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
