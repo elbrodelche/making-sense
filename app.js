3ea02c6aa301fdfed30fe021299aaf1468a57240
Juan Carlos Migliavacca <juancarlos@xeonworks.com>
2019-05-20 01:06:45 -0300

    Create, List, Search and Delete posts.

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 require('dotenv').config({
   path: `./env-files/${process.env.NODE_ENV || 'development'}.env`,
 });
-
+const debug = require('debug')('makingsense:app');
+const figlet = require('figlet');
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const { PORT } = process.env;
 const app = express();
-const port = process.env.PORT || 3000;
-const postRouter = express.Router();
+const port = PORT || '3000';
+const Post = require('./service/posts');
+const postRouter = require('./routes/postRouter')(Post);
 
-postRouter.route('/posts').get((req, res) => {
-  const response = { hello: 'Im a post' };
-  res.json(response);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 app.use(process.env.API_VERSION, postRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Making Sense API version 1.0.0. Status OK.');
 });
 
+app.set('port', port);
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  figlet('Making Sense', (err, data) => {
+    if (err) {
+      debug('Something went wrong...', err);
+      return;
+    }
+    console.log(data);
+    console.log(`Runing on port: ${port}`);
+  });
 });
---
+++ routes/postRouter.js
@@ -0,0 +1,76 @@
+const debug = require('debug')('app:postRouter');
+const express = require('express');
+
+const routes = (Post) => {
+  const postRouter = express.Router();
+
+  postRouter.route('/posts').get(async (req, res) => {
+    debug('List Posts');
+
+    const { query } = req;
+    // Available parameters
+    //   pgNum: '',
+    //   pgSize: '',
+    //   sort: '',
+    //   search: '',
+    //   status: '',
+    // Query Posts Service
+    const response = await Post.getPosts(query)
+    // Catch error and return 200 response
+      .catch((e) => {
+        debug(`getPosts call error ${e}`);
+        // Write error response
+        return res.json(e);
+      });
+
+    // Write success response
+    return res.json(response);
+  });
+  postRouter.route('/posts').put(async (req, res) => {
+    debug('Create Post');
+
+    const response = await Post.createPost(req.body)
+    // Catch error and return 200 response
+      .catch((e) => {
+        debug(`getPosts call error ${e}`);
+        // Write error response
+        return res.json(e);
+      });
+
+    // Write success response
+    return res.json(response);
+  });
+  postRouter.route('/posts/:postId').delete(async (req, res) => {
+    debug('Delete Posts');
+
+    // Query Posts Service
+    await Post.deletePost(req.params.postId)
+    // Catch error and return 200 response
+      .catch((e) => {
+        debug(`deletePost call error ${e}`);
+        // Write error response
+        return res.json(e);
+      });
+
+    // Write success response
+    return res.sendStatus(204);
+  });
+  postRouter.route('/posts/:postId').get(async (req, res) => {
+    debug('List Post by ID');
+    // Query Posts Service
+    const response = await Post.getPostById(req.params.postId)
+    // Catch error and return 200 response
+      .catch((e) => {
+        debug(`getPostById call error ${e}`);
+        // Write error response
+        return res.json(e);
+      });
+
+    // Write success response
+    return res.json(response);
+  });
+
+  return postRouter;
+};
+
+module.exports = routes;
